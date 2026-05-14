da7c1f934a7875244d551f15873f142200ee8c59
LaminatedYamal <audemardreamin@gmail.com>
2026-05-14 16:18:19 +0100

    v115: Real-time Telemetry Update [Special] - 16:18

---
+++ 404-board/results_special.js
@@ -0,0 +1,68 @@
+console.log('Telemetry Sync (Special): 2026-05-14 16:18:17'); window.specialData = [
+    {
+        "Url":  "https://www.ensinolusofona.pt",
+        "Course":  "Www.Ensinolusofona.Pt",
+        "Institution":  "Outros",
+        "StatusCode":  200,
+        "Status":  "OK",
+        "Message":  "HTTP 200",
+        "LastChecked":  "2026-05-14 16:18:16",
+        "FirstDownDate":  "",
+        "ConsecutiveDaysDown":  0
+    },
+    {
+        "Url":  "https://www.ismat.pt",
+        "Course":  "Www.Ismat.Pt",
+        "Institution":  "ISMAT",
+        "StatusCode":  200,
+        "Status":  "OK",
+        "Message":  "HTTP 200",
+        "LastChecked":  "2026-05-14 16:18:16",
+        "FirstDownDate":  "",
+        "ConsecutiveDaysDown":  0
+    },
+    {
+        "Url":  "https://www.ipluso.pt",
+        "Course":  "Www.Ipluso.Pt",
+        "Institution":  "IPLUSO",
+        "StatusCode":  200,
+        "Status":  "OK",
+        "Message":  "HTTP 200",
+        "LastChecked":  "2026-05-14 16:18:16",
+        "FirstDownDate":  "",
+        "ConsecutiveDaysDown":  0
+    },
+    {
+        "Url":  "https://www.islagaia.pt",
+        "Course":  "Www.Islagaia.Pt",
+        "Institution":  "ISLA Gaia",
+        "StatusCode":  200,
+        "Status":  "OK",
+        "Message":  "HTTP 200",
+        "LastChecked":  "2026-05-14 16:18:17",
+        "FirstDownDate":  "",
+        "ConsecutiveDaysDown":  0
+    },
+    {
+        "Url":  "https://www.islasantarem.pt",
+        "Course":  "Www.Islasantarem.Pt",
+        "Institution":  "Outros",
+        "StatusCode":  200,
+        "Status":  "OK",
+        "Message":  "HTTP 200",
+        "LastChecked":  "2026-05-14 16:18:17",
+        "FirstDownDate":  "",
+        "ConsecutiveDaysDown":  0
+    },
+    {
+        "Url":  "https://investigacao.ulusofona.pt",
+        "Course":  "Investigacao.Ulusofona.Pt",
+        "Institution":  "Outros",
+        "StatusCode":  200,
+        "Status":  "OK",
+        "Message":  "HTTP 200",
+        "LastChecked":  "2026-05-14 16:18:17",
+        "FirstDownDate":  "",
+        "ConsecutiveDaysDown":  0
+    }
+];
